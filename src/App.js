be0f59abb8b61b87db7c3a23fa2890d339966d06
Richard Cao <403164405@qq.com>
2016-12-16 20:16:29 +0800

    简陋版本

--- src/App.js
+++ src/App.js
@@ -1,20 +1,114 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import logo from './logo.svg';
+import { Button, Label } from 'react-bootstrap';
 import './App.css';
+import 'whatwg-fetch';
+import 'es6-promise';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      main: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://123.206.219.180:3000/where?mis=mashuangfei', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json, text/plain, */*'
+        },
+        mode: 'cors'
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data)
+      this.setState({
+        main: data
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <h2 className="Title" >在哪儿</h2>
+        <div className="Status">
+          <Button bsStyle="success">上班</Button>
         </div>
-        <Button bsStyle="primary">Test Button</Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Status">
+          <Button bsStyle="warning">暂离</Button>
+        </div>
+        <div className="Status">
+          <Button bsStyle="danger">下班</Button>
+        </div>
+        {this.state.main.seats.map((seat, index) => {
+            const seatView = (              
+              <div 
+                key={index}
+                className="Seat"
+              >
+              <Label className="Seat-Row">{(index + 1) + "排"}</Label>
+              {seat.map((user, index) => {
+                let userView = null;
+                switch (user.seatStatus) {
+                  case -1:
+                    userView = (
+                      <Button
+                        key={index}
+                        className="Seat-Unknow"
+                      >
+                      </Button>
+                    );
+                    break;
+                  case 0:
+                    userView = (
+                      <Button
+                        key={index}
+                        bsStyle="primary Seat-User"
+                      >
+                        {user.user.name}
+                      </Button>
+                    );
+                    break;
+                  case 1:
+                    userView = (
+                      <Button
+                        key={index}
+                        bsStyle="success Seat-User"
+                      >
+                        {user.user.name}
+                      </Button>
+                    );
+                    break;
+                  case 2:
+                    userView = (
+                      <Button
+                        key={index}
+                        bsStyle="warning Seat-User"
+                      >
+                        {user.user.name}
+                      </Button>
+                    );
+                    break;
+                  case 3:
+                    userView = (
+                      <Button
+                        key={index}
+                        bsStyle="danger Seat-User"
+                      >
+                        {user.user.name}
+                      </Button>
+                    );
+                    break;
+                   default:
+                    break;
+                }
+                return userView;
+              })}
+              </div>);
+            return seatView;
+          })}
       </div>
     );
   }
